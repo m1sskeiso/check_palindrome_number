98c72d77faa2ffffb9352e0185e1fef09de15fb4
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-23 18:55:38 +0700

    first

--- checking _palindrome_number.py
+++ checking _palindrome_number.py
@@ -1,5 +1,10 @@
 # Defining a function for checking a palindrome number
+def is_palindrome(number):
 # Convert the number to a string for easy comparison   
+        str_number = str(number)
+
 # Check if the string is the same when reversed
+        return str_number == str_number[::-1]
+
 # Test cases
 
